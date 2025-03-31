4a7a1aac90bd12a22557131ec9d4e0efd15aa998
Robin <156453367+hbgroca@users.noreply.github.com>
2025-03-31 15:55:53 +0200

    Update DataContext.cs

--- Data/Context/DataContext.cs
+++ Data/Context/DataContext.cs
@@ -8,4 +8,16 @@ public class DataContext(DbContextOptions<DataContext> options) : DbContext(opti
     public DbSet<SearchEntity> Searches { get; set; }
     public DbSet<ResponseEntity> Responses { get; set; }
     public DbSet<ContactTypeEntity> ContactType { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ContactTypeEntity>()
+            .HasData(
+                new ContactTypeEntity { Id = 1, ContactType = "Telefon" },
+                new ContactTypeEntity { Id = 2, ContactType = "E-post" },
+                new ContactTypeEntity { Id = 3, ContactType = "Personlig möte" },
+                new ContactTypeEntity { Id = 4, ContactType = "Video möte" },
+                new ContactTypeEntity { Id = 5, ContactType = "Intervju" }
+            );
+    }
 }
